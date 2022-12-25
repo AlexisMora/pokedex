e5c7e35c365388d177420515b97c7cb4d4592517
Alexis Mora <alexis.mora.sanchez@gmail.com>
2022-12-25 19:45:19 +0100

    ring improvement

--- src/components/TypeBadge.tsx
+++ src/components/TypeBadge.tsx
@@ -6,7 +6,7 @@ interface Props {
 
 const TypeBadge:FC<Props> = ({ type }) => {
   return (
-    <span className={`capitalize flex items-center px-3 py-2 text-xs font-bold leading-none text-blue-900 ring-2 ring-blue-900 bg-yellow-400 rounded-full`}>
+    <span className={`capitalize flex items-center px-3 py-2 text-xs font-bold leading-none text-white ring-2 ring-white bg-transparent rounded-full`}>
         {type}
     </span>
   )
