842d54ae7955b0afb60646ee6f7b88e0587091a6
Alexis Mora <alexis.mora.sanchez@gmail.com>
2022-12-25 19:47:17 +0100

    feat: now thats an improvement

--- src/components/TypeBadge.tsx
+++ src/components/TypeBadge.tsx
@@ -6,7 +6,7 @@ interface Props {
 
 const TypeBadge:FC<Props> = ({ type }) => {
   return (
-    <span className={`capitalize flex items-center px-3 py-2 text-xs font-bold leading-none text-white ring-2 ring-white bg-transparent rounded-full`}>
+    <span className={`capitalize flex items-center px-3 py-2 text-xs font-bold leading-none cursor-pointer hover:text-black text-white ring-2 hover:ring-black ring-white hover:bg-white bg-transparent rounded-full`}>
         {type}
     </span>
   )
