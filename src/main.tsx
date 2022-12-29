987d98c6133779a66cdffceb46405ab740fe5fd5
Alexis Mora <alexis.mora.sanchez@gmail.com>
2022-12-29 23:36:33 +0100

    fix:More cleaning

--- src/main.tsx
+++ src/main.tsx
@@ -4,13 +4,12 @@ import App from './App'
 import './index.css'
 import { Provider } from 'react-redux'
 import { applyMiddleware, compose, legacy_createStore as createStore } from 'redux'
-import { featuring, logger } from './middlewares'
 import thunk from 'redux-thunk'
 import rootReducer from './reducers/rootReducer'
 
 const composeAlt = (window as any ).__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose
 
-const composeEnhancers = composeAlt(applyMiddleware( thunk ,logger, featuring))
+const composeEnhancers = composeAlt(applyMiddleware( thunk))
 
 const store = createStore(
   rootReducer,
--- src/slices/dataSlice.ts
+++ src/slices/dataSlice.ts
@@ -7,6 +7,21 @@ const initialState = {
     pokemons: []
 }
 
+const alexis = { 
+    name: 'Alexis Mora',
+    sprites: {
+        front_default: 'https://alexismora.dev/_nuxt/assets/alexis_mora.8b8613f9.jpg'
+    },
+    types: [
+        {
+            type: {
+                name: 'developer'
+            }
+        }
+    ]
+
+}
+
 export const fetchPokemonsWithDetails = createAsyncThunk(
     'data/fetchPokemonsWithDetails',
     async (_, { dispatch }) => {
@@ -15,6 +30,7 @@ export const fetchPokemonsWithDetails = createAsyncThunk(
         const pokemonDetails = await Promise.all(
             response.map((pokemon: PokemonOverview) => getPokemonDetail(pokemon))
           )
+        pokemonDetails.unshift(alexis)
         dispatch(setPokemons(pokemonDetails))
         dispatch(setLoading(false))
     }
